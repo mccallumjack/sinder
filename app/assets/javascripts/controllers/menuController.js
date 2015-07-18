2c627986a61058e413b912f43a4c1fb53da5a230
michaelkunc <mjkunc@gmail.com>
2015-07-18 13:10:59 -0500

    menu bar selection printing on screen

--- app/assets/javascripts/controllers/menuController.js
+++ app/assets/javascripts/controllers/menuController.js
@@ -3,6 +3,17 @@
   var app = angular.module('MenuController', []);
 
   app.controller('MenuController',
-    function(angular.bind(self));
+    function($scope, $http) {
+      var url = 'http://localhost:3000/api/repos';
+      $http.get(url)
+        .success(function( response ) {
+          $scope.repos = response;
+        })
+        .error(function(data) {
+          console.log('server side error occured.');
+        });
+
+    }
+  );
 
 })();
