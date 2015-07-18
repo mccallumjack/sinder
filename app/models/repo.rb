aa2b306d58cc0fa4ee8fa84dcecefd3f9fc4b2c9
Joe Awad <joeaawad@gmail.com>
2015-07-18 11:33:16 -0500

    change model names to singular and fix repo id validation after having renamed migration column

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -2,5 +2,5 @@ class Repo < ActiveRecord::Base
   has_many :actions, class_name: "UserRepoAction"
   has_many :users, through: :actions
 
-  validates :id, uniqueness: true, presence: true
+  validates :github_repo_id, uniqueness: true, presence: true
 end
