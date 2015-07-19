923b8b2a960f82cf0566cd69b3f81d3fa6399b05
Joe Awad <joeaawad@gmail.com>
2015-07-19 16:02:08 -0500

    remind self that this is not ruby

--- app/models/user_repo_action.rb
+++ app/models/user_repo_action.rb
@@ -4,11 +4,11 @@ class UserRepoAction < ActiveRecord::Base
 
   validates :status, presence: true
 
-  def this.star(user_github_access_token, repo_full_name)
+  def self.star(user_github_access_token, repo_full_name)
     client = Octokit::Client.new(access_token: user_github_access_token)
     client.star(repo_full_name)
     user = User.find_by(github_access_token: user_github_access_token)
     repo = Repo.find_by(full_name: repo_full_name)
-    this.create(user: user, repo: repo, status: 1)
+    self.create(user: user, repo: repo, status: 1)
   end
 end
