b8ec7d8aae48c451d1014a2c97e6115cf2ba3c80
Amber Taniuchi <ambrela@gmail.com>
2015-07-19 13:02:19 -0500

    Fix secrets

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718174002) do
+ActiveRecord::Schema.define(version: 20150717225759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "repos", force: :cascade do |t|
     t.integer  "github_repo_id"
@@ -45,14 +44,9 @@ ActiveRecord::Schema.define(version: 20150718174002) do
 
   create_table "users", force: :cascade do |t|
     t.integer  "github_user_id"
-    t.string   "email",            null: false
-    t.string   "crypted_password"
-    t.string   "salt"
     t.string   "github_login"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
