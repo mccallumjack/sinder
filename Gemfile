4542ce1fe0d0d62a00dcd40f3cbd4c2356451869
Amber Taniuchi <ambrela@gmail.com>
2015-07-18 20:16:58 -0500

    Add Oauth route

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-gem "sorcery"
+gem "figaro"
 gem 'active_model_serializers'
 
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
   root :to => "homepage#index"
   get '/display' => "homepage#display"
 
-  post "oauth/callback" => "oauths#callback"
-  get "oauth/callback" => "oauths#callback" # for use with Github
-  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
-  delete "oauth/:provider" => "oauths#destroy", :as => :delete_oauth
-
   namespace :api, defaults: {format: :json} do
     resources :repos, only: [:index]
   end
