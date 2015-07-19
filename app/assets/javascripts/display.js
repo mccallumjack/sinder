6570b445e9a1b5d454407512e20155bbcb640647
Jack McCallum <mccallumjack@gmail.com>
2015-07-19 11:08:23 -0500

    comment out failing ajax listeners

--- app/assets/javascripts/display.js
+++ app/assets/javascripts/display.js
@@ -58,39 +58,38 @@ RepoController.prototype.renderRepo = function(repo){
   $(this.view.description).html(repo.description)
 }
 
-RepoController.prototype.loadNext = function(){
+RepoController.prototype.loadNext = function(e){
   var repo = this.repolist.repos.shift()
   this.renderRepo(repo)
 }
 
 RepoController.prototype.bindEvents = function(){
 
-  $('#lang-nav li').on('click', 'a', function(e){
-      e.preventDefault();
-      var lang = $(this).text();
-  });
+  // $('#lang-nav li').on('click', 'a', function(e){
+  //     e.preventDefault();
+  //     var lang = $(this).text();
+  // });
 
-  // posting the upvotes
-  $('#star').on('click', function(e){
-    e.preventDefault();
-    console.log("CLICKED STAR!")
-    var url = $(this).attr('href');
-    $.ajax({
-      url: url,
-      method: 'POST'
-    });
+  // // posting the upvotes
+  // $('#star').on('click', function(e){
+  //   e.preventDefault();
+  //   var url = $(this).attr('href');
+  //   $.ajax({
+  //     url: url,
+  //     method: 'POST'
+  //   });
 
-  });
+  // });
 
-  // posting the downvotes
-  $('#hide').on('click', function(e){
-    e.preventDefault();
-    var url = $(this).attr('href');
-    $.ajax({
-      url: url,
-      method: 'POST'
-    });
-  });
+  // // posting the downvotes
+  // $('#hide').on('click', function(e){
+  //   e.preventDefault();
+  //   var url = $(this).attr('href');
+  //   $.ajax({
+  //     url: url,
+  //     method: 'POST'
+  //   });
+  // });
 
   $('button').on('click', this.loadNext.bind(repoController))
 
