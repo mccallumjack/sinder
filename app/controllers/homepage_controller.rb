a666aba953f25ba7d461472017a06d87d5c55002
Amber Taniuchi <ambrela@gmail.com>
2015-07-19 16:47:55 -0500

    Remove methods from application helper

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,44 +1,11 @@
 class HomepageController < ApplicationController
-  include ApplicationHelper
+  CLIENT_ID = ENV["GITHUB_KEY"]
+  CLIENT_SECRET = ENV["GITHUB_SECRET"]
 
-  use Rack::Session::Pool, :cookie_only => false
+  # use Rack::Session::Pool, :cookie_only => false
 
   def index
-      # if !authenticated?
-      #   authenticate!
-      # else
-      #   access_token = session[:access_token]
-      #   scopes = []
-
-      #   begin
-      #     auth_result = RestClient.get('https://api.github.com/user',
-      #                                  {:params => {:access_token => access_token},
-      #                                   :accept => :json})
-      #   rescue => e
-      #     # request didn't succeed because the token was revoked so we
-      #     # invalidate the token stored in the session and render the
-      #     # index page so that the user can start the OAuth flow again
-
-      #   session[:access_token] = nil
-      #     return authenticate!
-      # end
-
-      # the request succeeded, so we check the list of current scopes
-      # if auth_result.headers.include? :x_oauth_scopes
-      #   scopes = auth_result.headers[:x_oauth_scopes].split(', ')
-      # end
-
-      # auth_result = JSON.parse(auth_result)
-
-      # if scopes.include? 'user:email'
-      #   auth_result['private_emails'] =
-      #     JSON.parse(RestClient.get('https://api.github.com/user/emails',
-      #                    {:params => {:access_token => access_token},
-      #                     :accept => :json}))
-      # end
-
-      # erb :advanced, :locals => auth_result
-    # end
+    @client_id = CLIENT_ID
   end
 
   def display
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
 
-  CLIENT_ID = ENV["GITHUB_KEY"]
-  CLIENT_SECRET = ENV["GITHUB_SECRET"]
-
-  def authenticated?
-    session[:access_token]
-  end
-
-  def authenticate!
-    @client_id = CLIENT_ID
-  end
 
 end
