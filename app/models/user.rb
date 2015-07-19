29e0340834443c0611fd5b7765d210298047c1c0
Amber Taniuchi <ambrela@gmail.com>
2015-07-19 11:15:58 -0500

    Removed more sorecery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,8 @@
 class User < ActiveRecord::Base
-  helper_method :has_linked_github?
-
-  authenticates_with_sorcery! do |config|
-    config.authentications_class = Authentication
-  end
 
   has_many :actions, class_name: "UserRepoAction"
   has_many :repos, through: :actions
 
-  has_many :authentications, :dependent => :destroy
-  accepts_nested_attributes_for :authentications
-
-
   validates :github_user_id, uniqueness: true, presence: true
-
-  def has_linked_github?
-    authentications.where(provider: 'github').present?
-  end
 
 end
--- spec/controllers/oauths_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe OauthsController, type: :controller do
-
-end
