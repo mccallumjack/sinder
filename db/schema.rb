ea155771a643b8379f8afea9c2628a652ddb7498
joeaawad <joeaawad@gmail.com>
2015-07-20 16:40:23 -0500

    add contributors file migration and fix seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717225759) do
+ActiveRecord::Schema.define(version: 20150720205529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150717225759) do
     t.integer  "pull_request_count"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "contributors_file"
   end
 
   create_table "user_repo_actions", force: :cascade do |t|
