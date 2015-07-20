336381f317a0c251943f94a28b2fd096a3d78484
Jack McCallum <mccallumjack@gmail.com>
2015-07-20 16:13:07 -0500

    add API tests

---
+++ spec/apis/repos_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+
+
+describe Api::ReposController, type: :controller do
+
+  describe 'GET#index' do
+
+    it 'responds with a 200' do
+      get :index, format: :json
+      expect(response).to be_success
+    end
+
+    it 'responds with a 200 when a language is specified' do
+      get :index,  {language: "ruby"}, format: :json
+      expect(response).to be_success
+    end
+
+
+  end
+
+
+  describe 'GET#issues' do
+
+    it 'responds with a 200 when a repo is passed' do
+      create(:repo)
+      repo = Repo.first
+      id = repo.id
+      get :issues, {id: id}
+      expect(response).to be_success
+    end
+
+  end
+
+
+end
