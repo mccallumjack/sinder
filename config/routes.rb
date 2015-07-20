ab3ae506f98c84517063a3ecd45ffc6f917ef16a
joeaawad <joeaawad@gmail.com>
2015-07-20 18:22:22 -0500

    add logout routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root :to => "homepage#index"
   get '/display' => "homepage#display"
   get '/github' => "homepage#github"
+  post '/logout' => "homepage#logout"
 
   get '/api/repos/:id/issues' => "api/repos#issues", defaults: { format: :json }
 
