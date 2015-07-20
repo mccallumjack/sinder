4b303c4b4010dbcfcb972049bad698cf82ff9870
Joe Awad <joeaawad@gmail.com>
2015-07-20 11:27:53 -0500

    debugging puts

--- app/controllers/user_repo_actions_controller.rb
+++ app/controllers/user_repo_actions_controller.rb
@@ -14,7 +14,9 @@ skip_before_action :verify_authenticity_token
   def hide
     if session[:access_token]
       repo = Repo.find_by(full_name: params[:repo_full_name])
+      puts repo
       user = User.find_by(github_access_token: session[:github_access_token])
+      puts user
       user_repo_action = UserRepoAction.create(user_id: user.id, repo_id: repo.id, status: 0)
     end
 
