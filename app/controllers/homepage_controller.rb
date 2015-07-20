df8a0df65227d90609a470f2107927a11a084866
Amber Taniuchi <ambrela@gmail.com>
2015-07-19 19:15:20 -0500

    parse

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -21,11 +21,12 @@ class HomepageController < ApplicationController
                            :client_secret => CLIENT_SECRET,
                            :code => params[:code]},
                            :accept => :json)
-    session[:access_token] = result['access_token']
+    session[:access_token] = JSON.parse(result)['access_token']
     puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+    # puts result['access_token']
     puts session[:access_token]
     puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-    # User.where(github_access_token: session[:access_token]).first_or_initialize
+    User.where(github_access_token: session[:access_token]).first_or_initialize
     redirect_to display_path
   end
 
