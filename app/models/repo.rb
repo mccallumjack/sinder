0dac98eb254c7b030a71487f974afbb10cc71b65
joeaawad <joeaawad@gmail.com>
2015-07-20 15:00:25 -0500

    add previous interaction filtering

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -3,4 +3,24 @@ class Repo < ActiveRecord::Base
   has_many :users, through: :actions
 
   validates :github_repo_id, uniqueness: true, presence: true
+
+  def self.all_repos(user)
+  	new_repos = []
+  	self.all.each do |repo|
+  		unless UserRepoAction.find_by(user_id: user.id, repo_id: repo.id)
+  			new_repos << repo
+  		end
+  	end
+  	return new_repos
+  end
+
+  def self.language_repos(user, language)
+  	language_repo_array = []
+  	self.all_repos(user).each do |repo|
+  		if repo.language == language
+  			language_repo_array << repo
+  		end
+  	end
+  	return language_repo_array
+  end
 end
