00325f63eeea79e10ab739208112ced2a8703761
Joe Awad <joeaawad@gmail.com>
2015-07-21 15:08:31 -0500

    add something that should work for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,17 @@ class User < ActiveRecord::Base
 
   validates :github_access_token, uniqueness: true, presence: true
 
+  def check_interactions
+    client = Octokit::Client.new(access_token: github_access_token)
+    check_starred(client.starred)
+    check_forked(client.)
+  end
+
+  def check_starred(starred_repos)
+    starred_repos.each do |repo|
+      if our_repo = Repo.find_by(github_repo_id: repo.id)
+        UserRepoAction.where(user: self, repo: our_repo, status: 1).first_or_create
+      end
+    end
+  end
 end
