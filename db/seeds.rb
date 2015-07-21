9cbe918f03fca67219c1c85e7dd91eaf97536014
Joe Awad <joeaawad@gmail.com>
2015-07-21 10:06:03 -0500

    add workers to automatically update database

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,7 +64,7 @@ end
 client = Octokit::Client.new(:client_id => ENV['GITHUB_KEY'], :client_secret => ENV['GITHUB_SECRET'])
 
 whitelist.each do |address|
-  repo = client.repo address
+  repo = client.repo(address)
   contributors_count = get_contributor_count(repo)
   contrib_file = contributing_file?(repo)
   pull_request_count = open_pull_requests(repo)
