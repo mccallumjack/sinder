cf69a7e7d12c978dadf2c725fb8fba41924173a9
Joe Awad <joeaawad@gmail.com>
2015-07-21 08:20:36 -0500

    committing old repo controller so I can pull

--- app/controllers/api/repos_controller.rb
+++ app/controllers/api/repos_controller.rb
@@ -7,6 +7,7 @@ module Api
   class ReposController < ApplicationController
 
     def index
+
       repos = params[:language] && params[:language] != "all languages" ? Repo.where("language ilike '#{params[:language]}'") : Repo.all
       render json: repos
     end
