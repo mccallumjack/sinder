aef562d767ee5d0fe569c2d83d6a2eddc9a05dfd
Amber Taniuchi <ambrela@gmail.com>
2015-07-22 13:19:50 -0500

    Add code_of_conduct column to Repo table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720205529) do
+ActiveRecord::Schema.define(version: 20150722181722) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150720205529) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "contributors_file"
+    t.boolean  "code_of_conduct"
   end
 
   create_table "user_repo_actions", force: :cascade do |t|
